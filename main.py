ca7afd928b686dc9126e5be89fd346a7f8a704db
CompSciMrF <17910442+geekyteacher@users.noreply.github.com>
2021-06-11 12:42:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# Please DON'T REQUEST EDIT ACCESS.
+
+#FORK ME or copy the code! This is the original so it needs to stay undedited for all users.
+
+# Task - Predict and Run
+
+#This is a comment. The computer ignores it. It's just here to give us humans a clue about what the code does.
+
+#Task 1 - Add a comment on the line above the 'print' command to predict what the code on line 12 will do.
+
+
+print("Hello World!")
+
+# Task Investigate
+
+# What would the output of the code print("I love Computing") be?
+
+# What would happen if the code print("I love Comping") was run?
+
+# What would happen if the code print("I love Computing" was run?
+
+
+# Task Modify
+
+# Reuse the code to output your own message. Add comments to show what it does.
+
+
+#Task Make
+# Output a joke that outputs on multiple lines. 
+print("""Hello this is the first line...
+another line
+and another
+""")
+
